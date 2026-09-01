78b519e7ec1d3c01586b7d644c6874d70d7ebc0c
k1rvl07 <kirkorol07@gmail.com>
2026-09-01 22:14:37 +0300

    feat: frontend scaffold

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-50 text-slate-900 dark:bg-slate-950 dark:text-slate-100">
+    <main>
+    </main>
+  </div>
+</template>
